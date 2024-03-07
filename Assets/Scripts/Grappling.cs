01e0704b77b536f8742a964cb7bd2911ae3519c3
Schafber <schafber@students.uni-marburg.de>
2024-03-07 15:47:50 +0100

    Grapplinghock cam

--- Assets/Scripts/Grappling.cs
+++ Assets/Scripts/Grappling.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Grappling : MonoBehaviour
 {
-    private SnakeMove sm;
+    private SnakeMove _sm;
+    private PlayerManagement _pm;
     public Transform orientation;
     public Transform tip;
     public LayerMask whatToGrapple;
@@ -12,78 +14,83 @@ public class Grappling : MonoBehaviour
 
 
     [SerializeField] private float maxDistance;
-    [SerializeField] private float DelayTime;
+    [SerializeField] private float delayTime;
     [SerializeField] private float overshootYAxis;
 
-    private Vector3 grapplePoint;
+    private Vector3 _grapplePoint;
 
-    private bool grappling;
+    private bool _grappling;
 
-    [SerializeField] private float grapplingCD;
-    private float grapplingCDTimer;
+    [SerializeField] private float grapplingCd;
+    private float _grapplingCdTimer;
 
     // Start is called before the first frame update
     void Start()
     {
         lr.enabled = false;
-        sm = GetComponent<SnakeMove>();
+        _sm = GetComponent<SnakeMove>();
     }
 
     public void StartGrapple()
     {
-        if (grapplingCDTimer > 0) return;
+        if (_grapplingCdTimer > 0) return;
 
-        grappling = true;
-
+        _grappling = true;
 
         RaycastHit hit;
-        if (Physics.Raycast(orientation.position, orientation.forward, out hit, maxDistance, whatToGrapple))
+        if (Physics.Raycast(_pm.GetCamTransform().position, _pm.GetCamDir(), out hit, maxDistance, whatToGrapple))
         {
-            grapplePoint = hit.point;
+            _grapplePoint = hit.point;
 
-            Invoke(nameof(ExecuteGrapple), DelayTime);
+            Invoke(nameof(ExecuteGrapple), delayTime);
         }
         else
         {
-            grapplePoint = orientation.position + orientation.forward * maxDistance;
+            
+            _grapplePoint = _pm.GetCamTransform().position + _pm.GetCamDir() * maxDistance;
 
-            Invoke(nameof(StopGrapple), DelayTime);
+            Invoke(nameof(StopGrapple), delayTime);
         }
 
         lr.enabled = true;
-        lr.SetPosition(1, grapplePoint);
+        lr.SetPosition(1, _grapplePoint);
     }
 
     public void ExecuteGrapple()
     {
-        float grapplePointRelativeY = grapplePoint.y - transform.position.y;
+        float grapplePointRelativeY = _grapplePoint.y - transform.position.y;
         float highestPointOnArc = Mathf.Max(grapplePointRelativeY, 0) + overshootYAxis;
 
-        sm.JumpToPosition(grapplePoint, highestPointOnArc);
+        _sm.JumpToPosition(_grapplePoint, highestPointOnArc);
 
         Invoke(nameof(StopGrapple), 1f);
     }
 
     public void StopGrapple()
     {
-        grappling = false;
-        grapplingCDTimer = grapplingCD;
+        _grappling = false;
+        _grapplingCdTimer = grapplingCd;
         lr.enabled = false;
     }
 
     private void Update()
     {
-        if (grapplingCDTimer > 0)
+        if (_grapplingCdTimer > 0)
         {
-            grapplingCDTimer -= Time.deltaTime;
+            _grapplingCdTimer -= Time.deltaTime;
         }
     }
 
     private void LateUpdate()
     {
-        if(grappling)
+        if(_grappling)
         {
             lr.SetPosition(0, tip.position);
         }
+    }
+
+    public void SetPm(PlayerManagement pm)
+    {
+        _pm = pm;
     }
 }
